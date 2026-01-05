5a257c7b375e827fcda9d30b175bb9475022491a
da4do0 <79230118+da4do0@users.noreply.github.com>
2026-01-05 11:58:27 +0100

    add robas to progettos

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.firstapp
+
+import android.R.attr.onClick
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.firstapp.exercises.Todo
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            AppNavigation(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun AppNavigation(navController: NavHostController){
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ){
+        composable("home"){
+            HomeScreen(navController)
+        }
+        composable("Todo"){
+            Todo(navController)
+        }
+    }
+}
+
+data class Exercise(
+    val title: String,
+    val description: String,
+    val route: String
+);
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavHostController){
+    val exercises = listOf(
+        Exercise("Esercizio 1", "Todo App - Liste e State", "Todo"),
+        Exercise("Esercizio 2", "Counter - State Management", "exercise2"),
+        Exercise("Esercizio 3", "Form - TextField e Validazione", "exercise3"),
+    )
+
+    Scaffold { paddingValues ->
+        Row(modifier = Modifier.padding(paddingValues)){
+            LazyColumn {
+                items(exercises){ exercise ->
+                    ExerciseCard(exercise, onclick = {navController.navigate(exercise.route)})
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ExerciseCard(exercise: Exercise, onclick: () -> Unit){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onclick() },
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = exercise.title,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = exercise.description,
+                    fontSize = 14.sp,
+                    color = Color.Gray
+                )
+            }
+
+            Text(
+                "Vai!"
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/firstapp/exercises/Todo.kt
@@ -0,0 +1,170 @@
+package com.example.firstapp.exercises
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.firstapp.R
+import kotlin.collections.plus
+
+data class Todo(val id: Int, val testo: String)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Todo(navController: NavHostController){
+    var testoTemp by remember { mutableStateOf("") }
+    var todos by remember { mutableStateOf(listOf<Todo>()) }
+    var nextId by remember { mutableStateOf(1) }
+
+    val buttonColor = if (testoTemp.isEmpty()) Color(0xFFB0B0B0) else Color(0xFF1E2732);
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Esercizio 1 - Todo App") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigateUp() }) {
+                        Text("Indietro")
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF2196F3),
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = Color.White
+                )
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .background(Color(0xFF1E1E1E))
+                .padding(8.dp)
+        ) {
+
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                items(
+                    items = todos,
+                    key = { it.id }
+                ) { todo ->
+                    Card(
+                        modifier = Modifier.fillMaxWidth().background(Color(0xFF1A1A1A)),
+                        shape = RoundedCornerShape(4.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(
+                                text = todo.testo,
+                                modifier = Modifier
+                                    .fillMaxHeight()
+                                    .weight(1f),
+                                fontSize = 16.sp
+                            )
+                            Button(
+                                onClick = {
+                                    todos = todos.filter { it.id != todo.id }
+                                },
+                                modifier = Modifier
+                                    .padding(4.dp, 2.dp).fillMaxHeight(),
+                                shape = RoundedCornerShape(0.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(0xFF1DB954)
+                                )
+                            ) {
+                                Text("✓")
+                            }
+                        }
+                    }
+                }
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(6.dp),
+                modifier = Modifier.height(50.dp)
+            ) {
+                TextField(
+                    onValueChange = { testoTemp = it },
+                    value = testoTemp,
+                    shape = RoundedCornerShape(12.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color(0xFFB0B0B0),
+                        unfocusedContainerColor = Color(0xFFB0B0B0),
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    placeholder = {
+                        Text(
+                            "Inserisci un nuovo",
+                            color = Color.Black
+                        )
+                    })
+
+                Button(
+                    onClick = {
+                        if (!testoTemp.isEmpty()) {
+                            todos = todos + Todo(nextId, testoTemp);
+                            nextId++;
+                            testoTemp = ""
+                        }
+                    },
+                    shape = RoundedCornerShape(6.dp),
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxHeight(),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = buttonColor
+                    )
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.plus),
+                        contentDescription = "Segno di spunta",
+                        modifier = Modifier.size(24.dp),
+                        tint = Color.LightGray
+                    )
+                }
+            }
+        }
+    }
+}
